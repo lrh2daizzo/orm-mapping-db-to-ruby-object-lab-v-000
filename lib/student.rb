32207c8df266924df3627e83e494703f8e106d57
Luis Herrera <herrera.lrh@gmail.com>
2020-01-23 17:00:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,7 @@
 require 'pry'
 class Student
-  attr_accessor :name, :grade
-  attr_reader :id
-  
+  attr_accessor :id, :name, :grade
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
