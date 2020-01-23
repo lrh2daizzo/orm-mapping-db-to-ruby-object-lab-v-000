fd026ee027d9224c79a15c91b70eec07905d8bf3
Luis Herrera <herrera.lrh@gmail.com>
2020-01-23 17:19:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,4 +57,15 @@ class Student
     DB[:conn].execute(sql, name).map { |row| self.new_from_db(row)
     }.first
   end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9;
+    SQL
+
+    DB[:conn].execute(sql).map { |row| self.new_from_db(row) }
+  end
+  
 end
